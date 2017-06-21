3a378d6d2023c1d5164acc6768ec1955a2a292d8
CristianG540 <cristianneatproy@gmail.com>
2017-06-21 16:35:55 -0500

    comento el codigo

--- classes/clsWebServiceHandle.php
+++ classes/clsWebServiceHandle.php
@@ -55,6 +55,10 @@ class WebServiceHandle {
         $this->log->pushHandler(new BrowserConsoleHandler());
     }
 
+    /**
+     * Este metodo es que se logue en el sap y recupera el id de la sesion
+     * @return string me retorna el id de la sesion, aun de todas formas lo asigna al valor protegido de la clase
+     */
     public function login() {
         $error  = $this->loginService->getError();
         if(!$error){
@@ -82,6 +86,12 @@ class WebServiceHandle {
         }
     }
 
+    /**
+     * Este metodo es el encargado de cerrar la sesion en sap, recibe el id de la
+     * sesion que se desea cerrar
+     * @param  string $sessionId Id de la sesion que se desea cerrar
+     * @return bool            me regresa un falso o un verdadero dependiendo de si se proceso o no el logout
+     */
     public function logout($sessionId = '') {
         $id = ($sessionId) ? $sessionId : $this->sessionId;
         $params = [
@@ -104,14 +114,32 @@ class WebServiceHandle {
         }
     }
 
+    /**
+     * El metodo order se encarga de enviar la peticion al servicio de ordenes del webservice
+     * y procesarla debidamente
+     * @param  array $order     Este array recibe la orden que llega desde la pagina de prestashop
+     * tiene los productos, la fecha en que se creo y el id de la orden
+     * @param  string $sessionId Opcionalmente se le puede enviar el id de la sesion en sap con el que
+     * se quiere procesar la orden
+     * @return bool            me regresa un falso o un verdadero dependiendo de si se proceso o no la orden
+     */
     public function order($order, $sessionId = '') {
         $id = ($sessionId) ? $sessionId : $this->sessionId;
+
+        /**
+         * Este metodo del webservice pide unos headers entonces los agrego
+         */
         $paramsH = [
             'SessionID'   => $id,
             'ServiceName' => 'OrdersService'
         ];
         $this->ordersService->setHeaders(['MsgHeader' => $paramsH]);
 
+        /**
+         * Con un reduce meto todos los productos de al array en un texto con el formato que pide el
+         * webservice
+         * @var array
+         */
         $products = array_reduce($order['productos'], function($carry, $item){
             $carry .= '<DocumentLine>'
                             . "<ItemCode>{$item['referencia']}</ItemCode>"
@@ -122,6 +150,9 @@ class WebServiceHandle {
 
         $error = $this->ordersService->getError();
         if(!$error){
+            /**
+             * Armo la estructura xml que le voy a enviar al metodo Add del webservice
+             */
             $soapRes = $this->ordersService->call('Add', ''
                     . '<Add>'
                         . '<Document>'
@@ -138,7 +169,7 @@ class WebServiceHandle {
                     );
 
             /**
-             * Me trae la peticion en xml pulpo de lo que se envio por soap al sap
+             * Me trae la peticion en xml crudo de lo que se envio por soap al sap
              * algo asi como soap envelope bla, bla
              */
             $this->log->info('Request orden es: '.$this->ordersService->request);
@@ -151,7 +182,8 @@ class WebServiceHandle {
              * para activarlo hay q setear el nivel de debug a mas de 0 ejemplo: "$this->ordersService->setDebugLevel(9);"
              */
             $this->log->info('Debug orden es: '.$this->ordersService->debug_str);
-            // Verifico que no haya ningun error
+            // Verifico que no haya ningun error, tambien reviso si existe exactamente la ruta del array que especifico
+            // si esa rut ano existe significa que algo raro paso muy posiblemente un error
             $error = $this->ordersService->getError();
             if($error || !isset($soapRes['DocumentParams']['DocEntry'])){
                 $this->log->error('Error al hacer el pedido SAP: '. json_encode($error) );
@@ -203,6 +235,11 @@ class WebServiceHandle {
         {
             case 'loginService':
                 $this->loginService = new nusoap_client($value, true);
+                /**
+                 * recibe un valor del 0 al 9 donde 0 es deshabilitado
+                 * con este "setDebugLevel" habilito que el nusoap recoja informacion sobre todo el proceso que realiza en el
+                 * webservice, para recuperar la informacion se usaria algo como: $this->ordersService->debug_str
+                 */
                 $this->loginService->setDebugLevel(0);
                 break;
             case 'ordersService':
